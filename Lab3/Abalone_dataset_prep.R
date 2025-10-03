30c5e633e2fe0600856f4abc4a5682fe8dabc070
ericee3 <122549954+ericee3@users.noreply.github.com>
2025-10-03 14:23:22 -0400

    lab3
    lab3 info

--- Lab3/Abalone_dataset_prep.R
+++ Lab3/Abalone_dataset_prep.R
@@ -2,15 +2,138 @@
 ##### Abalone Data Preparation #####
 ####################################
 
-# read dataset
+# Load required libraries
+library(ggplot2)   # For visualization
+library(readr)     # For reading CSV files
+library(class)     # For kNN classification
+library(caret)     # For confusion matrix calculation
+library(dplyr)     # For data manipulation
+
+# Read the dataset
 abalone.data <- read.csv("/Users/elizabethrice/Desktop/Data Analytics Labs/Lab3/abalone_dataset.csv")
+View(abalone.data)
 
-## add new column age.group with 3 values based on the number of rings 
-abalone.data$age.group <- cut(abalone.data$rings, br=c(0,8,11,35), labels = c("young", 'adult', 'old'))
+# Create a working copy of the dataset
+dataset <- abalone.data
 
-## alternative way of setting age.group
-abalone.data$age.group[abalone.data$rings<=8] <- "young"
-abalone.data$age.group[abalone.data$rings>8 & abalone.data$rings<=11] <- "adult"
-abalone.data$age.group[abalone.data$rings>11 & abalone.data$rings<=35] <- "old"
+# Add a new column 'age.group' based on number of rings
+# young: 0-8 rings, adult: 9-11 rings, old: 12-35 rings
+dataset$age.group <- cut(dataset$rings, breaks=c(0,8,11,35), labels=c("young", "adult", "old"))
 
+# Alternative method to assign age groups (overwrites previous column)
+dataset$age.group[dataset$rings <= 8] <- "young"
+dataset$age.group[dataset$rings > 8 & dataset$rings <= 11] <- "adult"
+dataset$age.group[dataset$rings > 11 & dataset$rings <= 35] <- "old"
+
+View(dataset)
+
+####################################
+# Exercise 1: kNN Classification
+####################################
+
+# Split data into 70% training and 30% testing
+n <- nrow(dataset)
+s_data <- sample(n, n*0.7)
+dataset.train <- dataset[s_data, ]
+dataset.test  <- dataset[-s_data, ]
+
+# Estimate a simple k value (square root of n)
+k <- round(sqrt(n))
+
+########################
+# Train & Evaluate kNN #
+########################
+
+# --- Model 1: using length, diameter, and height ---
+knn.predicted <- knn(
+  train = dataset.train[, 2:4],
+  test  = dataset.test[, 2:4],
+  cl    = dataset.train$age.group,
+  k = 65
+)
+
+# Create confusion matrix
+contingency.table <- table(knn.predicted, dataset.test$age.group, dnn = list("predicted","actual"))
+contingency.table
+
+# Calculate classification accuracy
+sum(diag(contingency.table)) / length(dataset.test$age.group)
+
+# --- Model 2: using weight features (whole, shucked, viscera, shell) ---
+knn.predicted <- knn(
+  train = dataset.train[, 5:8],
+  test  = dataset.test[, 5:8],
+  cl    = dataset.train$age.group,
+  k = 65
+)
+
+# Confusion matrix and accuracy
+contingency.table <- table(knn.predicted, dataset.test$age.group, dnn = list("predicted","actual"))
+contingency.table
+sum(diag(contingency.table)) / length(dataset.test$age.group)
+
+##########################
+# Find optimal k value
+##########################
+
+# Test multiple k values and store accuracies
+k.list <- c(45,55,60,65,75,85,105,155,205)
+accuracy.list <- c()
+
+for (k in k.list) {
+  knn.predicted <- knn(
+    train = dataset.train[,5:8],
+    test  = dataset.test[,5:8],
+    cl    = dataset.train$age.group,
+    k = k
+  )
+  
+  contingency.table <- table(knn.predicted, dataset.test$age.group, dnn=list('predicted','actual'))
+  accuracy <- sum(diag(contingency.table)) / length(dataset.test$age.group)
+  accuracy.list <- c(accuracy.list, accuracy)
+}
+
+# Plot accuracy vs k
+plot(k.list, accuracy.list, type = "b", xlab = "k", ylab = "Accuracy", main = "kNN Accuracy vs k")
+
+####################################
+# Exercise 2: K-Means Clustering
+####################################
+
+# Initial scatter plot of dataset colored by age group
+ggplot(dataset, aes(x = whole_weight, y = viscera_wieght, colour = age.group)) +
+  geom_point()
+
+# Run K-Means clustering with k = 3
+k <- 3
+data.km <- kmeans(dataset[,5:8], centers = k)
+
+# Store cluster assignments as factor
+assigned.clusters <- as.factor(data.km$cluster)
+
+# Plot again for visualization
+ggplot(dataset, aes(x = whole_weight, y = viscera_wieght, colour = age.group)) +
+  geom_point()
+
+# Print total within-cluster sum of squares
+data.km$tot.withinss
+data.km$cluster
+
+# Test multiple k values and plot WCSS
+k.list <- c(2,3,4,5,6)
+wcss.list <- c()
+
+for (k in k.list) {
+  data.km <- kmeans(dataset[,5:8], centers = k)
+  wcss <- data.km$tot.withinss
+  wcss.list <- c(wcss.list, wcss)
+  
+  # Optional: visualize clusters for each k
+  assigned.clusters <- as.factor(data.km$cluster)
+  ggplot(dataset, aes(x = whole_weight, y = viscera_wieght, colour = age.group)) +
+    geom_point()
+}
+
+# Plot WCSS vs number of clusters
+plot(k.list, wcss.list, type = "b", xlab = "Number of Clusters (k)", ylab = "WCSS", main = "K-Means WCSS vs k")
 
